34a5063bb7bf69a48bd44e42f6b477d4dc2e47bc
deise.mfarias <deise.mfarias@CAS555516W1019.senacsp.edu.br>
2019-08-16 21:36:59 -0300

    123 testando... Versão inicial sout

---
+++ gerenciador-produtos/src/main/java/titecnologia/pi3/gerenciadorprodutos/TesteGit.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author deise.mfarias
+ */
+public class TesteGit {
+    
+    public static void main(String[] args) {
+        
+        System.out.println(" 1 2 3 testando git...");
+    }
+    
+}
